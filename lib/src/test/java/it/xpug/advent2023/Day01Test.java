141091097ac8e5c8e715e1de3db738cbf84d5f52
Matteo Vaccari <mvaccari@thoughtworks.com>
2023-12-04 07:50:54 +0100

    Refactoring day01

--- lib/src/test/java/it/xpug/advent2023/Day01Test.java
+++ lib/src/test/java/it/xpug/advent2023/Day01Test.java
@@ -1,13 +1,15 @@
 package it.xpug.advent2023;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Optional;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static java.lang.Character.isDigit;
@@ -16,9 +18,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Day01Test {
 
     private Integer day01(Stream<String> input) {
-        Function<Integer, Integer> timesTen = n -> n*10;
+        Function<Integer, Integer> timesTen = n -> n * 10;
         Function<Integer, Integer> id = n -> n;
-        Function<Pair<Integer, Integer>, Integer> sum = nn -> nn.left() + nn.right();
+        Function<Pair<Integer, Integer>, Integer> sum = pair -> pair.left() + pair.right();
 
         Function<String, Integer> processLine =
                 Pair.split(this::firstDigitInString, this::lastDigitInString)
@@ -41,34 +43,38 @@ public class Day01Test {
             "nine",
     };
 
-    private Integer firstDigitInString(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (isDigit(s.charAt(i))) {
-                return s.charAt(i) - '0';
-            }
-            String startingAtIndex = s.substring(i);
-            for (int j = 1; j < DIGITS.length; j++) {
-                if (startingAtIndex.startsWith(DIGITS[j])) {
-                    return j;
-                }
+    private Optional<Integer> startsWithDigit(String s) {
+        if (isDigit(s.charAt(0))) {
+            return Optional.of(s.charAt(0) - '0');
+        }
+        for (int j = 1; j < DIGITS.length; j++) {
+            if (s.startsWith(DIGITS[j])) {
+                return Optional.of(j);
             }
         }
-        throw new IllegalStateException("Not found");
+        return Optional.empty();
+    }
+
+    private Integer firstDigitInString(String s) {
+        return IntStream.range(0, s.length())
+                .mapToObj(s::substring)
+                .flatMap(ss -> startsWithDigit(ss).stream())
+                .findFirst()
+                .orElseThrow();
+    }
+
+    // Thanks https://stackoverflow.com/a/24011264/164802
+    static IntStream reverseRange(int from, int to) {
+        return IntStream.range(from, to)
+                .map(i -> to - i + from - 1);
     }
 
     private Integer lastDigitInString(String s) {
-        for (int i = s.length()-1; i >= 0; i--) {
-            if (isDigit(s.charAt(i))) {
-                return s.charAt(i) - '0';
-            }
-            String startingAtIndex = s.substring(i);
-            for (int j = 1; j < DIGITS.length; j++) {
-                if (startingAtIndex.startsWith(DIGITS[j])) {
-                    return j;
-                }
-            }
-        }
-        throw new IllegalStateException("Not found");
+        return reverseRange(0, s.length())
+                .mapToObj(s::substring)
+                .flatMap(ss -> startsWithDigit(ss).stream())
+                .findFirst()
+                .orElseThrow();
     }
 
     @Test
@@ -86,32 +92,27 @@ public class Day01Test {
     @Test
     void acceptance2() {
         var input = """
-two1nine
-eightwothree
-abcone2threexyz
-xtwone3four
-4nineeightseven2
-zoneight234
-7pqrstsixteen
-""";
+                two1nine
+                eightwothree
+                abcone2threexyz
+                xtwone3four
+                4nineeightseven2
+                zoneight234
+                7pqrstsixteen
+                """;
 
         assertThat(day01(Arrays.stream(input.split("\n")))).isEqualTo(281);
     }
 
     @Test
-    void real() throws IOException {
+    void solution() throws IOException {
         var result = day01(Files.lines(Path.of("src/main/resources/day01.txt")));
         System.out.println(result);
     }
 
-    @Test
     void testFirstDigitInString() {
         assertThat(firstDigitInString("1abc")).isEqualTo(1);
         assertThat(firstDigitInString("sss2abc")).isEqualTo(2);
-    }
-
-    @Test
-    void testFirstNumberInWords() {
         assertThat(firstDigitInString("two1nine")).isEqualTo(2);
         assertThat(firstDigitInString("abcone2threexyz")).isEqualTo(1);
     }
@@ -120,12 +121,9 @@ zoneight234
     void testLastDigitInString() {
         assertThat(lastDigitInString("1a4bc")).isEqualTo(4);
         assertThat(lastDigitInString("sss2abc")).isEqualTo(2);
-    }
-
-    @Test
-    void testLastDigitInWords() {
         assertThat(lastDigitInString("4nineeightseven2")).isEqualTo(2);
         assertThat(lastDigitInString("4nineeightseven")).isEqualTo(7);
         assertThat(lastDigitInString("4nineeightaaaa")).isEqualTo(8);
     }
+
 }
